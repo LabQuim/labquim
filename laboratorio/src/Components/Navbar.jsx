8a9b5e19bcf57734b92ee619e138f009afb23ba5
Andres <carlosandressuarez16@gmail.com>
2022-01-28 10:40:27 -0500

    despliegue heroku

--- laboratorio/src/Components/Navbar.jsx
+++ laboratorio/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
         <div className='Navbar bg-green-500 text-center'>
-            <h3>Hola soy el navbar</h3>
+            <h3>Hola soy el navbar desplegado</h3>
         </div>
 
   );
