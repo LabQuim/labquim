0885dcc16e23743f8602fb86df5cb30f972ee9d4
Andres <carlosandressuarez16@gmail.com>
2022-01-28 10:20:43 -0500

    organizacion archivos landing page

---
+++ laboratorio/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+        <div className='Navbar bg-green-500 text-center'>
+            <h3>Hola soy el navbar</h3>
+        </div>
+
+  );
+};
+
+export default Navbar;
---
+++ laboratorio/src/Pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from 'Components/Navbar';
+
+const home = () => {
+  return (
+  <div>
+      <Navbar/>
+      <p>Hola soy el contenido de la pagina home</p>
+  </div>);
+};
+
+export default home;
