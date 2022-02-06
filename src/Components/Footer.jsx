0065a4de7b87f9f3ca9d3cec867a6b31f0c1dec0
Andrés Suárez <carlosandressuarez16@gmail.com>
2022-02-06 17:38:58 -0500

    footer and design

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import ContactButton from 'Components/ContactButton'
+import { NavLink } from 'react-router-dom'
+
+
+const Footer = () => {
+    return (
+        <div className=' bg-gray-600 w-full flex flex-row text-center h-40 text-10xl'>
+            <div className='flex '>
+                <div className=' m-2 mt-5'>
+                    Aqui va el logo
+                </div>
+                <div className=''>
+                    <NavbarLinks/>
+                </div>
+                <div >
+                    <ContactButton text='Contáctanos' />
+                </div>
+
+                
+                            
+                    
+            </div>
+        </div>
+
+        
+    )
+}
+
+const NavbarLinks = () =>{
+    return(
+        <ul className='flex flex-nowrap auto-rows-auto justify-items-center content-between p-5 self-auto text-10xl  '>
+            <NavbarRoute to='/planes' title='Planes' />
+            <NavbarRoute to='/nosotros' title='Nosotros' />
+            <NavbarRoute to='/blog' title='Blog' />
+            <NavbarRoute to='' title='' />
+            
+
+        </ul>
+
+    );
+};
+
+const NavbarRoute = ({to, title, icon})=>{
+    return(
+        <li>
+            <NavLink
+                to={to}
+                className={({ isActive }) =>
+                isActive
+                    ? ' navbar-route border-t-2  border-b-2 font-extrabold border-green text-sky-700'
+                    : ' navbar-route font-bold  text-white hover:text-black hover:bg-green-700'
+                }
+            >
+                <div className='flex items-center'>
+                    <i className={icon} />
+                    <span className='text-2xl self-auto mx-10 font-["verdana"] '>{title}</span>
+                </div>
+            </NavLink>
+        </li>
+    )
+
+}
+
+export default Footer
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from 'Components/Navbar'
 import Banner from 'Components/Banner';
 import Agronomy from 'Media/Agronomy.jpg';
+import Footer from 'Components/Footer';
 
 const home = () => {
   return (
@@ -15,6 +16,62 @@ const home = () => {
                 image={Agronomy}
             />
             <h3 className="text-center font-bold text-4xl mt-6">Beneficios</h3>
+            <div className='flex flex-row justify-center space-x-12 mt-16 h-full '>
+
+                                <div className='bg-white rounded-xl w-80 hover:scale-110 transition-transform mb-10' >
+                                    <div className='flex flex-col col-span-3 '>
+                                        <div>
+                                            {/* <img src={} alt='imagen de movil' className='p-7'></img> */}
+                                            <div className='text-center mb-6 font-bold text-2xl p-5'>
+                                                <br />
+                                                Evita desplazamientos innecesarios
+                                                <br />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='bg-white rounded-xl w-80 hover:scale-110 transition-transform mb-10' >
+                                    <div className='flex flex-col col-span-3 '>
+                                        <div>
+                                            {/* <img src={} alt='imagen de movil' className='p-7'></img> */}
+                                            <div className='text-center mb-6 font-bold text-2xl p-5'>
+                                                <br />
+                                                Tiempos de análisis reduicidos
+                                                <br />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='bg-white rounded-xl w-80 hover:scale-110 transition-transform mb-10' >
+                                    <div className='flex flex-col col-span-3 '>
+                                        <div>
+                                            {/* <img src={} alt='imagen de movil' className='p-7'></img> */}
+                                            <div className='text-center mb-6 font-bold text-2xl p-5'>
+                                                <br />
+                                                se entrega manual de instrucciones para la toma de muestras y posterior análisis
+                                                <br />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='bg-white rounded-xl w-80 hover:scale-110 transition-transform mb-10' >
+                                    <div className='flex flex-col col-span-3 '>
+                                        <div>
+                                            {/* <img src={} alt='imagen de movil' className='p-7'></img> */}
+                                            <div className='text-center mb-6 font-bold text-2xl p-5'>
+                                                <br />
+                                                kit con instrumentos y material necesario para el análisis e interpretación de resultados
+                                                <br />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                    </div>
+                    <Footer/>
   </div>);
 };
 
