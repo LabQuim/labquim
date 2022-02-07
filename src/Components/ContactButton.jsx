0c49d05f5408716c607ae3c1e9a2c57ca31227c2
Andres <carlosandressuarez16@gmail.com>
2022-02-07 14:41:38 -0500

    hover y banner radiant

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactButton = ({ text }) => {
     return (
         
-            <button  className='text-center space-y-5 text-white align-top font-bold m-6 border-none rounded-lg h-20 p-5 bg-green-800' >
+            <button  className='text-center space-y-5 text-white align-top font-bold m-6 border-none rounded-lg h-20 p-5 bg-green-800 hover:scale-110 transition-transform' >
                 {text}
             </button>
         
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
         <div className='text-center text-4xl'>
             
             <div className='w-auto h-auto m-auto '> 
-                <img className='w-2/4 h-2/4 m-auto text-center' src={Logo} alt="Logo Laboratorio" />
+                <img className='w-2/4 h-2/5 m-auto text-center' src={Logo} alt="Logo Laboratorio" />
             </div>
             <div className='w-2/4 h-0.5 p-1 m-auto text-center'>
                 <hr className='justify-center w-full p-1 bg-gradient-to-r from-green-700 to-orange-600'/>
             </div>
 
-            <div className='bg-green-400 rounded-lg flex flex-col text-center text-4xl w-full mt-2'>
+            <div className='bg-gradient-to-b from-green-700 to-green-300 rounded-lg flex flex-col text-center text-4xl w-full mt-2'>
                 
                 <NavbarLinks/>
             </div>
@@ -54,7 +54,7 @@ const NavbarRoute = ({to, title, icon})=>{
                 className={({ isActive }) =>
                 isActive
                     ? ' navbar-route border-t-2  border-b-2 font-extrabold border-green text-blue-400'
-                    : ' navbar-route font-bold  text-gray-700 hover:text-black hover:bg-green-700'
+                    : ' navbar-route font-bold  text-white hover:text-black hover:underline'
                 }
             >
                 <div >
