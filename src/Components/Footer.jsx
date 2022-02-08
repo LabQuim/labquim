207adf28f928059519890824b21ae6e5eaa29b69
Andres <carlosandressuarez16@gmail.com>
2022-02-07 21:03:59 -0500

    logo blanco

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ContactButton from 'Components/ContactButton'
 import { NavLink } from 'react-router-dom'
-import Logo from 'Media/logomb.png'
+import Logo2 from 'Media/logomblanco.png';
 
 const Footer = () => {
     return (
         <div className=' bg-gray-600 w-full flex flex-row text-center h-40 text-10xl'>
             <div className='flex '>
             <div className='w-auto h-auto m-auto text-left'> 
-                <img className='w-3/5 h-3/5 ml-5 text-center' src={Logo} alt="Logo Laboratorio" />
+                <img className='w-3/5 h-3/5 ml-5 text-center' src={Logo2} alt="Logo Laboratorio" />
             </div>
                 <div className=''>
                     <NavbarLinks/>
