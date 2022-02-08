6b936d0ea8ee5e95ccdf98911af9d2e942877cb6
Andres <carlosandressuarez16@gmail.com>
2022-02-07 20:40:57 -0500

    logo footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ContactButton from 'Components/ContactButton'
 import { NavLink } from 'react-router-dom'
-
+import Logo from 'Media/logomb.png'
 
 const Footer = () => {
     return (
         <div className=' bg-gray-600 w-full flex flex-row text-center h-40 text-10xl'>
             <div className='flex '>
-                <div className=' m-2 mt-5'>
-                    Aqui va el logo
-                </div>
+            <div className='w-auto h-auto m-auto text-left'> 
+                <img className='w-3/5 h-3/5 ml-5 text-center' src={Logo} alt="Logo Laboratorio" />
+            </div>
                 <div className=''>
                     <NavbarLinks/>
                 </div>
