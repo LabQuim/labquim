23ee7e02b9467c44f76a946d386450ba349c702f
Andres <carlosandressuarez16@gmail.com>
2022-02-10 15:29:46 -0500

    imagenes infocards

--- src/Components/Infocard.jsx
+++ src/Components/Infocard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Infocard = ({ text, image }) => {
   return (
-    <div className="div1 bg-gray-300 rounded-xl w-full h-48 sm:w-full sm:h-56 md:h-96 align-middle font-bold sm:m-1 hover:scale-110 transition-transform sm:text-center sm:justify-center ">
-      <div className='texto  m-auto p-5 h-full text-center align-middle sm:text-center sm:items-center sm:text-2xl text-xl'>
-        <img className='sm:w-60 sm:h-60 sm:text-center sm:items-center' src={image} alt="imagen de tarjetas" />
+    <div className="div1 bg-gray-300 rounded-xl w-full h-auto sm:w-full sm:h-auto md:h-90 align-middle font-semibold sm:m-auto hover:scale-110 transition-transform sm:text-center sm:justify-center ">
+      <div className='texto  m-auto p-3 h-full text-center align-middle sm:text-center sm:items-center sm:text-2xl text-xl'>
+        <img className='w-28 h-28 m-auto sm:w-60 sm:h-60 sm:text-center sm:justify-center sm:m-auto' src={image} alt="imagen de tarjetas" />
         <p className='sm:text-center sm:justify-center sm:m-2 sm:h-full'>{text}</p>
       </div>
         
