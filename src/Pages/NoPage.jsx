515b617c4138f9badda36bcc98c452044b6d41b4
Andrés Suárez <carlosandressuarez16@gmail.com>
2022-02-11 14:12:45 -0500

    No page

--- src/Pages/NoPage.jsx
+++ src/Pages/NoPage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NoPage = () => {
   return (
-    <div className='sm:text-center sm:my-10 text-center'>
-        <p>Error 404</p>
-        <h1 className='font-bold sm:font-bold sm:text-2xl sm:mt-20 font='>Page not found</h1>
-        <h3>La pagina que estas buscando no existe o ha sido eliminada</h3>
+    <div className='text-center sm:text-center my-28 sm:my-28 sm:text-2xl p-5'>
+        <p className='font-bold'>Error 404</p>
+        <h1 className='font-bold sm:font-bold sm:text-2xl mt-20 sm:mt-40'>PAGE NOT FOUND</h1>
+        <h3 className='mt-5 '>La página que estás buscando no existe o ha sido eliminada</h3>
         <h4>Porfavor verifica la URL o dirección.</h4>
     </div>
   )
