4815bea099f8b596c3d1355de68e1defb04a70e1
Andres <carlosandressuarez16@gmail.com>
2022-02-12 14:36:47 -0500

    formulario typeform

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Widget } from '@typeform/embed-react'
 
 
 const Formulario = () => {
      
   return (
-    <div data-tf-widget="Lh3e0nBk" data-tf-hide-headers data-tf-iframe-props="title=Registration Form" data-tf-medium="snippet"  
-        style={{width:"100%", height:"600px"}}>
-    </div>
+    <Widget id="Lh3e0nBk" style={{ width:"100%", height:"600px" }} className="my-form" />
+    // <div data-tf-widget="Lh3e0nBk" data-tf-hide-headers data-tf-iframe-props="title=Registration Form" data-tf-medium="snippet"  
+    //     style={{width:"100%", height:"600px"}}>
+    // </div>
   )
 }
 
